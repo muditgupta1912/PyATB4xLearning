5671c822bc95991c19ecb13eb5eb04538caadbf3
muditgupta1912 <muditgupta1912@gmail.com>
2024-08-09 22:16:23 +0530

    initial commit !!

--- src/ex_08082024/lab001.py
+++ src/ex_08082024/lab001.py
@@ -1,6 +1,11 @@
 # this is comment , line which will not run !!
-print("welcome Mudit, to python an amazing language !! and you nailed this language!!")
-print("2+2")
-print(2-2+8)
+#print("welcome Mudit, to python an amazing language !! and you nailed this language!!")
+#print("2+2")
+#print(2-2+8)
+
+
+print("tomorrow, I am going Balaji dham for Prabhu Darshan!")
+print(2+19)
+print(2-19)
 
 
